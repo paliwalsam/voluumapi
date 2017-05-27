8cd8a500e3391e18df38b10d689c3986014210ca
paliwalsam <paliwal.sam@gmail.com>
2017-05-27 16:03:01 +0530

    test commit

--- test.php
+++ test.php
@@ -5,8 +5,8 @@ use paliwalnitin\voluumwrap\voluumwrap;
 $vrc = new voluumwrap();
 $vrc->login('contact@mytokri.com', '@>#:e\\3&AZQqNN_k');
 
-//$campaigns = $vrc->getActiveCampaigns();
-//print_r( $campaigns );
+$campaigns = $vrc->getActiveCampaigns();
+print_r( $campaigns );
 
 $campaignId = '3d38aa4f-db62-4aef-958d-fac71fe936b2';
 $stats = $vrc->campaignReport($campaignId, 'last-30-days', '1');
